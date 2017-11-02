a0ff972f88e741ab0fd3193e36a98a0fb6ae0818
Alex Banister <alexbanister@gmail.com>
2017-11-02 10:48:56 -0600

    action test

---
+++ src/actions/index.test.js
@@ -0,0 +1,46 @@
+import * as actions from './CardCatelogActions';
+
+describe('CardCatalog Actions', () => {
+
+  it('addRecentMovies should take and array and return an action', () => {
+    const allHouses = [{
+      url:"https://www.anapioficeandfire.com/api/houses/81",
+      name:"House Corbray of Heart's Home",
+      coatOfArms:"Three black ravens in flight",
+      words:"",
+      titles:[
+        "Lord of Heart's Home",
+        "King of the Fingers (historical)"
+      ],
+      founded:"",
+      ancestralWeapons:[
+        "Lady Forlorn"
+      ],
+      swornMembers:[
+        "https://www.anapioficeandfire.com/api/characters/1182"
+      ]
+    }];
+    const expected = {
+      type: 'UPDATE_HOUSES',
+      allHouses: [{
+        url:"https://www.anapioficeandfire.com/api/houses/81",
+        name:"House Corbray of Heart's Home",
+        coatOfArms:"Three black ravens in flight",
+        words:"",
+        titles:[
+          "Lord of Heart's Home",
+          "King of the Fingers (historical)"
+        ],
+        founded:"",
+        ancestralWeapons:[
+          "Lady Forlorn"
+        ],
+        swornMembers:[
+          "https://www.anapioficeandfire.com/api/characters/1182"
+        ]
+      }]
+    };
+
+    expect(actions.updateHouses(allHouses)).toEqual(expected);
+  });
+});
