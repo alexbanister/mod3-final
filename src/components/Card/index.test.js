62914a7e9c5a52753075a697a67b765ea495ce92
Alex Banister <alexbanister@gmail.com>
2017-11-02 13:31:40 -0600

    add sworn member test

--- src/components/Card/index.test.js
+++ src/components/Card/index.test.js
@@ -25,4 +25,52 @@ describe('Card snapshot', () => {
 
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('should have default state', () => {
+    const house = {
+      name:"House Corbray of Heart's Home",
+      coatOfArms:"Three black ravens in flight",
+      words:"",
+      titles:[
+        "Lord of Heart's Home",
+        "King of the Fingers (historical)"
+      ],
+      founded:"",
+      seats:[
+        "Heart's Home"
+      ],
+      ancestralWeapons:[
+        "Lady Forlorn"
+      ]
+    };
+    const wrapper = shallow(<Card house={house} />);
+
+    expect(wrapper.instance().props.house).toEqual(house);
+  });
+
+  // it('should have display sworn members', () => {
+  //   const house = {
+  //     name:"House Corbray of Heart's Home",
+  //     coatOfArms:"Three black ravens in flight",
+  //     words:"",
+  //     titles:[
+  //       "Lord of Heart's Home",
+  //       "King of the Fingers (historical)"
+  //     ],
+  //     founded:"",
+  //     seats:[
+  //       "Heart's Home"
+  //     ],
+  //     ancestralWeapons:[
+  //       "Lady Forlorn"
+  //     ]
+  //   };
+  //   const wrapper = shallow(<Card house={house} />);
+  //
+  //   expect(wrapper.instance().props.house).toEqual(house);
+  //
+  //   wrapper.find('button').simulate('click')
+  //
+  //   expect(wrapper.instance().props.house).toEqual(house);
+  // });
 });
