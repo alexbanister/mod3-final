69558db074787544e13e22ac518200de58a98211
Alex Banister <alexbanister@gmail.com>
2017-11-02 12:55:25 -0600

    rough in sworn members api call

--- src/api.js
+++ src/api.js
@@ -3,3 +3,15 @@ export const getHouses = () => {
     .then(response => response.json())
     .then(parsedResponse => parsedResponse);
 };
+
+export const fetchSwornMember = (members) => {
+  const allMembers = members.map( url => {
+    return fetch('http://localhost:3001/api/v1/character', {
+      method: 'POST',
+      body: JSON.stringify({ url: url })
+    })
+      .then(response => response.json())
+      .then(parsedResponse => parsedResponse);
+  });
+  return Promise.all(allMembers);
+};
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,26 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { fetchSwornMember } from '../../api';
 import './styles.css';
 
-const Card = ({ house }) => {
-  const displayLists = (list) => {
+export class Card extends Component {
+  constructor(){
+    super();
+    this.state = {
+      swornMembers: [],
+      showSwornMembers: false
+    };
+  }
+
+  showSwornMembers() {
+    if (this.state.showSwornMembers) {
+      return <h4>hi</h4>;
+    }
+  }
+
+  async getSwornMember(membersURLs) {
+    const allMembers = await fetchSwornMember(membersURLs)
+    console.log(allMembers);
+    // this.setState({
+    //   swornMembers: membersURLs,
+    //   showSwornMembers: true
+    // });
+  }
+
+  displayLists = (list) => {
     return list.map( item => {
       return <p key={item}>{item}</p>;
     });
   };
 
-  return (
-    <div className='card'>
-      <h3>{house.name}</h3>
-      <h3>{house.words}</h3>
-      <h3>{house.founded}</h3>
-      <h3>{house.coatOfArms}</h3>
-      <h3>{displayLists(house.seats)}</h3>
-      <h3>{displayLists(house.titles)}</h3>
-      <h3>{displayLists(house.ancestralWeapons)}</h3>
-    </div>
-  );
-};
+  render() {
+    return (
+      <div className='card'>
+        <h3>{this.props.house.name}</h3>
+        <h3>{this.props.house.words}</h3>
+        <h3>{this.props.house.founded}</h3>
+        <h3>{this.props.house.coatOfArms}</h3>
+        <h3>{this.displayLists(this.props.house.seats)}</h3>
+        <h3>{this.displayLists(this.props.house.titles)}</h3>
+        <h3>{this.displayLists(this.props.house.ancestralWeapons)}</h3>
+        <button
+          onClick={()=>{ this.getSwornMember(this.props.house.swornMembers); }}>
+          Sworn Members
+        </button>
+        {this.showSwornMembers()}
+      </div>
+    );
+  }
+}
 
 Card.propTypes = {
   house: PropTypes.object
